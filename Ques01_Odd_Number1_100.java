a3f586d29007425f5f2ea6c7d29a22c3eaeae5ec
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-07-25 19:00:10 +0530

    Add files via upload

---
+++ Ques01_Odd_Number1_100.java
@@ -0,0 +1,12 @@
+package Numericals_100_Question;
+
+public class Ques01_Odd_Number1_100 {
+    public static void main(String[] args) {
+        System.out.println("Odd number is : ");
+        for(int i=1;i<=100;i++){
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
