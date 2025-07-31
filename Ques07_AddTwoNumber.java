23575bb65f663b3b742dfb18a45da673324c000b
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-07-31 09:47:51 +0530

    Add files via upload

---
+++ Ques07_AddTwoNumber.java
@@ -0,0 +1,8 @@
+package Numericals_100_Question;
+
+public class Ques07_AddTwoNumber {
+    public static void main(String[] args) {
+        int a=8,b=12;
+        System.out.println("Sum of a and b is :"+(a+b));
+    }
+}
---
+++ Ques08_SubtractTwoNumber.java
@@ -0,0 +1,8 @@
+package Numericals_100_Question;
+
+public class Ques08_SubtractTwoNumber {
+    public static void main(String[] args) {
+        int a=44,b=22;
+        System.out.println("Sub of a and b is : "+(a-b));
+    }
+}
