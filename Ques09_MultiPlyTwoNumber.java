cb9e2931e6e2455a335f8e116a15e7a358f2d130
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-08-01 10:57:54 +0530

    Add files via upload

---
+++ Ques09_MultiPlyTwoNumber.java
@@ -0,0 +1,8 @@
+package Numericals_100_Question;
+
+public class Ques09_MultiPlyTwoNumber {
+    public static void main(String[] args) {
+        int a=5,b=8;
+        System.out.println("Multiple of a and b is : "+(a*b));
+    }
+}
---
+++ Ques10_DivideTwoNumber.java
@@ -0,0 +1,8 @@
+package Numericals_100_Question;
+
+public class Ques10_DivideTwoNumber {
+    public static void main(String[] args) {
+        int a=40,b=5;
+        System.out.println("Divide of a and b is : "+(a/b));
+    }
+}
