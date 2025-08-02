acf3b9207bbb87d07d842b34e1436256d1fb30b0
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-08-02 12:58:44 +0530

    Add files via upload

---
+++ Ques11_Find_Remainder.java
@@ -0,0 +1,8 @@
+package Numericals_100_Question;
+
+public class Ques11_Find_Remainder {
+    public static void main(String[] args) {
+        int a=17,b=4;
+        System.out.println("Remainder is : "+(a%b));
+    }
+}
---
+++ Ques12_AverageOf_TwoNumber.java
@@ -0,0 +1,8 @@
+package Numericals_100_Question;
+
+public class Ques12_AverageOf_TwoNumber {
+    public static void main(String[] args) {
+        int a=9,b=99;
+        System.out.println("Average is : "+((a+b)/2));
+    }
+}
