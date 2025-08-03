71e9817543804fdeeb7e6047116819155d9c1f10
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-08-03 11:25:46 +0530

    Add files via upload

---
+++ Ques13_SquareOfNumber.java
@@ -0,0 +1,8 @@
+package Numericals_100_Question;
+
+public class Ques13_SquareOfNumber {
+    public static void main(String[] args) {
+        int a=5;
+        System.out.println("Square of number is : "+(a*a));
+    }
+}
---
+++ Ques14_CubeOfNumber.java
@@ -0,0 +1,13 @@
+package Numericals_100_Question;
+
+import java.util.Scanner;
+
+public class Ques14_CubeOfNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a num");
+        int cube=sc.nextInt();
+//        int num=5;
+        System.out.println("Cube of num is : "+ (cube*cube*cube));
+    }
+}
