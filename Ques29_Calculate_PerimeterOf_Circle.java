e9c505876c46bdbf0adb3b2f269003039e59b25a
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-08-12 21:57:17 +0530

    Add files via upload

---
+++ Ques29_Calculate_PerimeterOf_Circle.java
@@ -0,0 +1,12 @@
+package Numericals_100_Question;
+
+import java.util.Scanner;
+
+public class Ques29_Calculate_PerimeterOf_Circle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter radius : ");
+        double rad= sc.nextDouble();;
+        System.out.println("Perimeter : "+(2*3.14156*rad));
+    }
+}
---
+++ Ques30_MultiplyFloatAnd_Integer.java
@@ -0,0 +1,11 @@
+package Numericals_100_Question;
+
+import java.util.Scanner;
+
+public class Ques30_MultiplyFloatAnd_Integer {
+    public static void main(String[] args) {
+        float a=4.5f;
+        int b=5;
+        System.out.println("Multiply : " +(a*b));
+    }
+}
