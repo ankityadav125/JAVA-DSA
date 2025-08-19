0d518081fed6f66ba54127d87023e3a4b07008fb
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-08-19 22:39:34 +0530

    Add files via upload

---
+++ Ques41_CheckIfNumberIs_Positive_Negative.java
@@ -0,0 +1,12 @@
+package Numericals_100_Question;
+
+public class Ques41_CheckIfNumberIs_Positive_Negative {
+    public static void main(String[] args) {
+        int num=10;
+        if(num>=0){
+            System.out.println("Positive");
+        } else{
+            System.out.println("Negative");
+        }
+    }
+}
