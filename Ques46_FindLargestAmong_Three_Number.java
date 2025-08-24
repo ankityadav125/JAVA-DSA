e2a4f6a21f5944235e19e27194bc8133fbfed37f
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-08-24 16:08:24 +0530

    Add files via upload

---
+++ Ques46_FindLargestAmong_Three_Number.java
@@ -0,0 +1,22 @@
+package Numericals_100_Question;
+
+import java.util.Scanner;
+
+public class Ques46_FindLargestAmong_Three_Number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter num1 : ");
+        int num1=sc.nextInt();
+        System.out.print("Enter num2 : ");
+        int num2=sc.nextInt();
+        System.out.print("Enter num3 : ");
+        int num3=sc.nextInt();
+        if(num1>num2 && num1>num3){
+            System.out.println(num1 +" is greater");
+        } else if (num2>num3 && num2>num1) {
+            System.out.println(num2 +" is greater");
+        } else{
+            System.out.println(num3 +" is greater");
+        }
+    }
+}
