91c0932d006a802f042de8c9ed259cbc9cad289f
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-08-28 17:49:21 +0530

    Add files via upload

---
+++ Ques50_Check_CharacterIs_UppercaseOR_Lowercase.java
@@ -0,0 +1,12 @@
+package Numericals_100_Question;
+
+public class Ques50_Check_CharacterIs_UppercaseOR_Lowercase {
+    public static void main(String[] args) {
+        char ch='A';
+        if(Character.isUpperCase(ch)){
+            System.out.println("Uppercase");
+        } else{
+            System.out.println("Lowercase");
+        }
+    }
+}
